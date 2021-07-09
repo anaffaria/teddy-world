31458c2e3f27199b4ed9de14a270016a2f280cda
aninhafsilva <anacarolinq.ng@gmail.com>
2021-07-09 15:24:46 -0300

    style: changed showcase image

--- src/components/Showcase/Showcase.tsx
+++ src/components/Showcase/Showcase.tsx
@@ -1,6 +1,6 @@
 import Img1 from "../../components/Showcase/img/img1.jpg";
-import Img2 from "../../components/Showcase/img/img2.jpg";
-import Img3 from "../../components/Showcase/img/img3.jpg";
+import Img2 from "../../components/Showcase/img/img2.png";
+
 import "./Showcase.css";
 import { Carousel } from "react-bootstrap";
 
@@ -13,9 +13,6 @@ function Showcase() {
         </Carousel.Item>
         <Carousel.Item>
           <img className="d-block w-100" src={Img2} alt="Second slide" />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100" src={Img3} alt="Third slide" />
         </Carousel.Item>
       </Carousel>
     </>
