f2dd59bee14f3202fd908343d778513277f6e8c5
aninhafsilva <anacarolinq.ng@gmail.com>
2021-07-21 23:19:08 -0300

    style(adminnavbar): finished style

--- src/components/AdminNavBar/AdminNavBar.tsx
+++ src/components/AdminNavBar/AdminNavBar.tsx
@@ -1,33 +1,39 @@
 import AdminLogo from "../../assets/logoAdmin.svg";
 import "./AdminNavBar.css";
-import { GoGraph } from "react-icons/go";
-import { GiExitDoor } from "react-icons/gi";
-import { RiDoorOpenFill } from "react-icons/ri";
 import { IoExitOutline } from "react-icons/io5";
 
-function AdminNavBar() {
+interface AdminNavBarProps {
+  links?: Array<{
+    icon: JSX.Element;
+    text: string;
+  }>;
+}
+
+function AdminNavBar({ links }: AdminNavBarProps) {
   return (
     <nav className="navbar-container">
       <div className="navbar-admin">
         <div className="navbar-header">
-          <figure>
-            <img src={AdminLogo} alt="logo" />
+          <figure className="logo-wrapper">
+            <img src={AdminLogo} alt="logo" className="admin-logo" />
           </figure>
         </div>
 
         <hr className="divider" />
 
         <div className="navbar-contents-wrapper">
-          <div className="navbar-content">
-            <div className="navbar-content-icon">
-              <GoGraph fontSize={24}></GoGraph>
-            </div>
-            <div className="navbar-content-text">
-              <span>Dashboard</span>
-            </div>
-          </div>
+          {links?.map((element, index) => {
+            return (
+              <div className="navbar-content mb-4" key={index}>
+                <div className="navbar-content-icon">{element.icon}</div>
+                <div className="navbar-content-text">
+                  <span>{element.text}</span>
+                </div>
+              </div>
+            );
+          })}
         </div>
-        
+
         <div className="navbar-footer">
           <div className="navbar-footer-icon">
             <IoExitOutline fontSize={24}></IoExitOutline>
--- src/pages/Admin/Home/Home.tsx
+++ src/pages/Admin/Home/Home.tsx
@@ -1,10 +1,33 @@
+import { FiShoppingBag } from "react-icons/fi";
+import { GoGraph } from "react-icons/go";
+import { RiUserHeartLine } from "react-icons/ri";
+import { CgArrowsExchangeAlt } from "react-icons/cg";
 import AdminNavBar from "../../../components/AdminNavBar/AdminNavBar";
 
 function AdminIndex() {
+  const links = [
+    {
+      icon: <GoGraph fontSize={24}></GoGraph>,
+      text: "Dashboard",
+    },
+    {
+      icon: <FiShoppingBag fontSize={24}></FiShoppingBag>,
+      text: "Pedidos",
+    },
+    {
+      icon: <CgArrowsExchangeAlt fontSize={24}></CgArrowsExchangeAlt>,
+      text: "Devoluções",
+    },
+    {
+      icon: <RiUserHeartLine fontSize={24}></RiUserHeartLine>,
+      text: "Clientes",
+    },
+  ];
+
   return (
     <>
       <div className="d-flex">
-        <AdminNavBar />
+        <AdminNavBar links={links} />
         <h1>Holá AdminIndex :P</h1>
       </div>
     </>
