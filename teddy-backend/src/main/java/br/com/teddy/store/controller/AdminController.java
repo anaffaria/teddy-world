5a2036f185bc70ba588cd20d46dfd9b32654eaae
aninhafsilva <anacarolinq.ng@gmail.com>
2021-10-06 20:48:30 -0300

    refactor: backend

--- teddy-backend/src/main/java/br/com/teddy/store/controller/AdminController.java
+++ teddy-backend/src/main/java/br/com/teddy/store/controller/AdminController.java
@@ -1,2 +1,16 @@
-package br.com.teddy.store.controller;public class AdminController {
+package br.com.teddy.store.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    @GetMapping("")
+    public ResponseEntity getAdmin() {
+        return ResponseEntity.ok("");
+    }
 }
